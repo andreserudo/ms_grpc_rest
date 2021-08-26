2a89fe2af89c98564fe7a2526b9784b4773d0510
Serudo <andreserudo@gmail.com>
2021-08-25 22:09:10 -0400

    fix: visualizando o proto

--- hidra/src/index.js
+++ hidra/src/index.js
@@ -3,11 +3,17 @@ const grpc = require('grpc');
 
 const protoLoader = require('@grpc/proto-loader');
 
-const packageDefinition = protoLoader.loadSync(path.resolve(__dirname, 'pb'),{
-    keepCase: true,
-    longs: String,
-    enums: String,
-    defaults: true,
-    oneofs: true,
-});
+const packageDefinition = protoLoader.loadSync(
+    path.resolve(__dirname, 'pb','messages.proto'),
+    {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true,
+    }
+);
+
 const proto = grpc.loadPackageDefinition(packageDefinition);
+
+console.log(proto);
